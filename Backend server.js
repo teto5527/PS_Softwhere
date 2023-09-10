11d65eeb3834777f68671958874e86bd1b260849
Anthony Beckett <anthonyjbeckett@protonmail.com>
2023-09-10 17:55:38 +0930

    Add ability to sign-up

--- Backend server.js
+++ Backend server.js
@@ -3,16 +3,31 @@ const express = require('express'); // Importing the Express framework
 const bodyParser = require('body-parser'); // Middleware for parsing request bodies
 const sqlite3 = require('sqlite3').verbose(); // SQLite3 module with verbose logging enabled
 const bcrypt = require('bcrypt');
-const path = require("path"); // Library for hashing passwords
+const path = require('path'); // Library for hashing passwords
+const fs = require('fs');
 
 // Initialize an Express app
 const app = express();
 
 // Initialize a new SQLite3 database connection
-const db = new sqlite3.Database('database_path_here.db');
+const db = new sqlite3.Database('database.db');
+
+// Script that fills the database with schema
+const script = fs.readFileSync('database.sql', 'utf8');
 
 // Define the port number
 const PORT = 3000;
+
+// Execute the SQL script to create the database
+db.serialize(() => {
+    db.exec(script, function (err) {
+        if (err) {
+            console.error('Error executing SQL script:', err.message);
+        } else {
+            console.log('Database created successfully.');
+        }
+    });
+});
 
 app.get('/', function(req,res){
     res.sendFile(path.join(__dirname,'./HomePage.html'));
@@ -20,15 +35,6 @@ app.get('/', function(req,res){
 app.get('/login', function(req,res){
     res.sendFile(path.join(__dirname,'./login.html'));
 });
-
-app.post('/login', bodyParser.urlencoded(), (req, res) => {
-    const name = req.body.name;
-    const email = req.body.email;
-    const password = req.body.password;
-    console.log(name, email, password);
-});
-
-
 
 // Route for handling login requests
 // app.post('/login', (req, res) => {
@@ -51,25 +57,30 @@ app.post('/login', bodyParser.urlencoded(), (req, res) => {
 //     });
 // });
 //
-// // Route for handling signup requests
-// app.post('/signup', (req, res) => {
-//     const name = req.body.name;
-//     const email = req.body.email;
-//     // Hash the password using bcrypt
-//     const password = bcrypt.hashSync(req.body.password, 10);
-//
-//     // Insert the new user's email and hashed password into the database
-//     db.run("INSERT INTO user (email, password) VALUES (?, ?)", [email, password], function(err) {
-//         if (err) {
-//             res.status(500).json({error: err.message});
-//             return;
-//         }
-//         res.json({status: "success", message: "Registered successfully"});
-//     });
-// });
+
+
+// Route for handling signup requests
+app.post('/login', bodyParser.urlencoded(), (req, res) => {
+    const name = req.body.name;
+    const email = req.body.email;
+    // Generate salt for password
+    const salt = bcrypt.genSaltSync(10);
+    // Hash the password using bcrypt
+    const password = bcrypt.hashSync(req.body.password, salt);
+
+    // Insert the new user's email and hashed password into the database
+    db.run("INSERT INTO user (email, password) VALUES (?, ?)", [email, password], function(err) {
+        if (err) {
+            res.status(500).json({error: err.message});
+            return;
+        }
+        res.json({status: "success", message: "Registered successfully"});
+    });
+});
 
 
 // Start the Express server
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
+    console.log(`http://localhost:${PORT}`);
 });
